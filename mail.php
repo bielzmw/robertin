1a62c70b10ea0cbe0ddb463cdecb67a03f40d43d
strolly <101291851+bielzmw@users.noreply.github.com>
2022-05-10 16:58:19 -0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+Olá <?php echo $_POST["name"]; ?>  <?php echo $_POST["name2"]; ?><br>
+seu email é <?php echo $_POST["mail"]; ?> e sua senha é <?php echo $_POST["pass"]; ?><br>
+seu numero de telefone é: <?php echo $_POST["phone"]; ?> e seu cep é: <?php echo $_POST["cep1"]; ?><br>
+seu sexo é: <?php echo $_POST["sexo"]; ?>
+</body>
+</html>
